11ddb110e7894069f3b823665054cbc0e2ee00ca
Tudor <tualso.8969@gmail.com>
2026-06-11 14:46:32 +0200

    Remove private dataspace header label

--- src/components/Header/Menu.tsx
+++ src/components/Header/Menu.tsx
@@ -102,7 +102,6 @@ export default function Menu(): ReactElement {
         <Link href="/" className={styles.logo}>
           <Logo />
         </Link>
-        <div className={styles.demoText}>Private Dataspace</div>
 
         {/* <ul className={styles.navigation}>
         {siteContent?.menu.map((item: MenuItem) => (
